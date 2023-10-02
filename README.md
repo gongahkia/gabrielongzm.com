79802eb12d8969e397693860316517c01ffbc01c
gongahkia <angryapplegravy@gmail.com>
2023-10-02 21:45:37 +0800

    total revamp

--- README.md
+++ README.md
@@ -4,24 +4,18 @@
 
 ---
 
-<h3>Why a version 2?</h3>
+### Why a version 2?
 
 I wanted to try my hand at creating a less visually cluttered site, that had a focus on simplicity and responsive web design. 
 
-<h3>Technologies used</h3>
+### Technologies used
 
 ![TypeScript](https://img.shields.io/badge/-TypeScript-000?&logo=TypeScript)
 ![HTML](https://img.shields.io/badge/-HTML-000?&logo=html5)
 ![CSS](https://img.shields.io/badge/-CSS-000?&logo=css3)
 ![Github](https://img.shields.io/badge/-Github%20Sites-000?&logo=github)
 
-<h3>To do</h3>
-
-* [ ] implement smooth transitioning for social icons  
-* [ ] add moving background  
-* [ ] add working mobile port with touch-specific controls
-
-<h3>Resources</h3>
+### Resources
 
 * [Icons](https://simpleicons.org/?q=smile)
 * [Visual reference](https://www.adamalston.com/)
--- ts/app.ts
+++ ts/app.ts
@@ -1,15 +1,14 @@
 const theButton = document.getElementById("toggleButton");
 theButton?.addEventListener("click", pressTheButton);
 
-
 function pressTheButton() {
     const mainFella = document.getElementById("mainBody");
     const currentMode = mainFella?.getAttributeNode("class")!;
     const githubPic = document.getElementById("githubImg")!;
     const linkedinPic = document.getElementById("linkedinImg")!;
     const wordpressPic = document.getElementById("wordpressImg")!;
-    const jekyllPic = document.getElementById("jekyllImg")!;
     const gmailPic = document.getElementById("gmailImg")!;
+    const rmarkdownPic = document.getElementById("RmarkdownImg")!;
     const theImageThisTime = document.getElementById("toggleButton")!;
     if (currentMode.nodeValue == "lightMode") {
         mainFella?.removeAttribute("class");
@@ -17,8 +16,8 @@ function pressTheButton() {
         githubPic?.setAttribute("style", "filter:invert(1);");
         linkedinPic?.setAttribute("style", "filter:invert(1);");
         wordpressPic?.setAttribute("style", "filter:invert(1);");
-        jekyllPic?.setAttribute("style", "filter:invert(1);");
         gmailPic?.setAttribute("style", "filter:invert(1);");
+        rmarkdownPic?.setAttribute("style", "filter:invert(1);");
         theImageThisTime.setAttribute("src", "assets/moon.svg");
     } else if (currentMode.nodeValue == "darkMode") {
         mainFella?.removeAttribute("class");
@@ -26,8 +25,8 @@ function pressTheButton() {
         githubPic.removeAttribute("style");
         linkedinPic.removeAttribute("style");
         wordpressPic.removeAttribute("style");
-        jekyllPic.removeAttribute("style");
         gmailPic.removeAttribute("style");
+        rmarkdownPic.removeAttribute("style");
         theImageThisTime.setAttribute("src", "assets/sun.svg");
     }
     console.log(currentMode);

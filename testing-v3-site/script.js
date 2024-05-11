793d8c7756dae67b8a0d43ed2cc1f91c1236552b
gongahkia <angryapplegravy@gmail.com>
2024-05-11 14:30:21 +0800

    huat ah things are looking UP boys

--- testing-v3-site/script.js
+++ testing-v3-site/script.js
@@ -1,90 +1,62 @@
 // fua
     // try migrating code from app.js to script.js here to interact with styles.css
     // need to examine element names on index.html and edit them accordingly
+    // also work out how to add smoothing between the colors and text changing color
+    // rework tolerance for light and dark?
+    // the picture is inverting color now too; prevent that
+    // invert the article header also
+    // change color of my svg icon in css or html, its too bright right now
 
 "use strict";
 
-// toggle light and dark mode
-addEventListener("DOMContentLoaded", (e) => {
-    const darkModeCheckbox = document.getElementById("dark-mode");
-    darkModeCheckbox.addEventListener('change', (e) => {
-        localStorage.setItem('darkMode', JSON.stringify(e.target.checked));
-    });
-    let darkMode = false;
-    if (localStorage.getItem('darkMode') !== null) {
-        try {
-            darkMode = !!JSON.parse(localStorage.getItem('darkMode'));
-        } catch (e) {
-            console.error(e);
-        }
-    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        darkMode = true;
-    }
-    darkModeCheckbox.checked = darkMode;
-});
+// // toggle light and dark mode
+// addEventListener("DOMContentLoaded", (e) => {
+//     const darkModeCheckbox = document.getElementById("dark-mode");
+//     darkModeCheckbox.addEventListener('change', (e) => {
+//         localStorage.setItem('darkMode', JSON.stringify(e.target.checked));
+//     });
+//     let darkMode = false;
+//     if (localStorage.getItem('darkMode') !== null) {
+//         try {
+//             darkMode = !!JSON.parse(localStorage.getItem('darkMode'));
+//         } catch (e) {
+//             console.error(e);
+//         }
+//     } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//         darkMode = true;
+//     }
+//     darkModeCheckbox.checked = darkMode;
+// });
 
 // --- EDIT THIS ENTIRE BOTTOM PORTION ---
 
 const theButton = document.getElementById("infinityButton");
 theButton === null || theButton === void 0 ? void 0 : theButton.addEventListener("click", pressTheButton);
+
+// --- function definition ---
+
 function pressTheButton() {
-    console.log("firm"); // the function is running
+
     const randomColor = rngHexColor();
-    console.log(randomColor, checkHexDarkness(randomColor));
-    document.body.style.backgroundColor = randomColor; 
+    console.log(randomColor, checkHexDarkness(randomColor)); // test function is running
+    document.body.style.backgroundColor = randomColor; // change background color
+
+    // making use of dom selector
+    const articleTag = document.getElementsByClassName("overallArticleTags");
+    const imageTag = document.getElementById("gongImage")
+
+    // color change
+    if (checkHexDarkness(randomColor)){ // background darker
+        theButton.setAttribute("style", "filter:invert(1);");
+        articleTag[0].setAttribute("style", "filter:invert(1);");
+        imageTag.setAttribute("style", "filter:invert(1)"); // wtf some boolean inversion magic
+    } else { // background lighter
+        theButton.style.filter = "none";
+        articleTag[0].style.filter = "none";
+        imageTag.style.filter = "none";
+    }
 
 }
-
-    // get document id of each and every elment and assign it to a const in format of 
-    // const myNamePara = document.getElementById("my-name");
-
-//     const myNamePara = document.getElementById("my-name");
-//     const myDescPara = document.getElementById("my-description");
-//     const myCredNamePara = document.getElementById("my-credits-name");
-//     const myCredGithubPara = document.getElementById("my-credits-github");
-//     const myCredResumePara = document.getElementById("my-credits-resume");
-//     const myCredJapOnePara = document.getElementById("my-japanese-is-bad");
-//     const myCredJapTwoPara = document.getElementById("my-japanese-is-good");
-
-//     // these HTML elements will change when the button is pressed if the color is detected
-//     const mainFella = document.getElementById("mainBody");
-//     const currentMode = mainFella === null || mainFella === void 0 ? void 0 : mainFella.getAttributeNode("class");
-//     const githubPic = document.getElementById("githubImg");
-//     const linkedinPic = document.getElementById("linkedinImg");
-//     const wordpressPic = document.getElementById("wordpressImg");
-//     const gmailPic = document.getElementById("gmailImg");
-//     const infinityPic = document.getElementById("infinityButton");
-
-
-//     // ! asserts that a variable is non-nullable and is defined
-//     // the class "rotated" must be added to the element everytime it is to be played
-
-//     infinityPic.classList.add('rotated');
-//     if (checkHexDarkness(randomColor)) { // if relatively darker
-//         mainFella.removeAttribute("class");
-//         githubPic.setAttribute("style", "filter:invert(1);");
-//         linkedinPic.setAttribute("style", "filter:invert(1);");
-//         wordpressPic.setAttribute("style", "filter:invert(1);");
-//         gmailPic.setAttribute("style", "filter:invert(1);");
-//         infinityPic.setAttribute("style", "filter:invert(1);");
-//     }
-//     else { // if relatively light
-//         mainFella.removeAttribute("class");
-//         githubPic.removeAttribute("style");
-//         linkedinPic.removeAttribute("style");
-//         wordpressPic.removeAttribute("style");
-//         gmailPic.removeAttribute("style");
-//         infinityPic.removeAttribute("style");
-//     }
-
-//     // setTimeout() ensures the animation has cleared its entire cycle first before removing it
-//     setTimeout(() => {
-//         infinityPic.classList.remove('rotated');
-//     }, 750);
-
-// }
-
-// // --- helper functions ---
 
 function rngHexColor() {
     const letters = '0123456789ABCDEF';
